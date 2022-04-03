90179a2040efa543c0dc7a245cdf6ac8049197f7
Rishi Magiawala <rishimagiawala@gmail.com>
2022-04-02 20:35:32 -0400

    Web3

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import {ethEnabled} from './wallet'
 
 createRoot(document.getElementById("root")).render(<App />);
+
+ethEnabled()
--- src/wallet.js
+++ src/wallet.js
@@ -1,11 +1,11 @@
 const Web3 = require("web3");
-const ethEnabled = async () => {
+export const ethEnabled = async () => {
+  console.log('This is running')
   if (window.ethereum) {
     await window.ethereum.request({method: 'eth_requestAccounts'});
-    window.web3 = new Web3(window.ethereum);
+   window.web3 = new Web3(window.ethereum);
     return true;
   }
   return false;
 }
 
-ethEnabled();
