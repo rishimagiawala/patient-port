22f5fabb63e96e9d4c5bcf0c8b7a98df62408763
Rishi Magiawala <rishimagiawala@gmail.com>
2022-04-02 21:43:10 -0400

    hello

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import initWallet from './wallet.js'
+
 
 createRoot(document.getElementById("root")).render(<App />);
+initWallet()
---
+++ src/wallet.js
@@ -0,0 +1,15 @@
+const { ethers } = require("ethers");
+// A Web3Provider wraps a standard Web3 provider, which is
+// what MetaMask injects as window.ethereum into each page
+export let provider;
+export default function initWallet() {
+   provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  // MetaMask requires requesting permission to connect users accounts
+  provider.send("eth_requestAccounts", []);
+
+  // The MetaMask plugin also allows signing transactions to
+  // send ether and pay to change state within the blockchain.
+  // For this, you need the account signer...
+  const signer = provider.getSigner();
+}
