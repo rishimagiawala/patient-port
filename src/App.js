7b6174dcbef39116d772143c84150471d2e6f559
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2022-04-02 18:26:51 -0700

    Update code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import Web3 from "web3";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
 const App = () => {
-  
   return (
     <ChakraProvider>
       <BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {ethEnabled} from './wallet'
 
 createRoot(document.getElementById("root")).render(<App />);
-
-ethEnabled()
